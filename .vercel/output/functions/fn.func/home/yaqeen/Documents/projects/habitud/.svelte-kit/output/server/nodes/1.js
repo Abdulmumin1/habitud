09b56ef14f99e717f484272dbcb5725a84a2ec06
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-10-30 10:32:09 +0100

    feat: pwa

--- .vercel/output/functions/fn.func/home/yaqeen/Documents/projects/habitud/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.91327b62.js","_app/immutable/chunks/scheduler.f9761f20.js","_app/immutable/chunks/index.ff8ba8f7.js","_app/immutable/chunks/stores.4d9db4ba.js","_app/immutable/chunks/singletons.b69217b0.js","_app/immutable/chunks/index.7ad99811.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/home/yaqeen/Documents/projects/habitud/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.8c93b6b5.js","_app/immutable/chunks/scheduler.f9761f20.js","_app/immutable/chunks/index.ff8ba8f7.js","_app/immutable/chunks/streakcard.6a4fe4e7.js","_app/immutable/chunks/each.f852ad0c.js"];
-export const stylesheets = ["_app/immutable/assets/streakcard.9b4b71c8.css"];
-export const fonts = [];
