918d2993240c2a2d387f7b0110e7aa7bf658a094
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2025-11-16 19:20:30 +0100

    fix: source

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client/edge';
 import { env } from '$env/dynamic/private'
 
 
-const prisma = new PrismaClient({ datasourceUrl: env.DATABASE_URL }).$extends(withAccelerate());
+const prisma = new PrismaClient({ datasourceUrl: env.DATABASE_URL })
 
 export { prisma };
