bc164560c8e528a73c6a34a34409f673df227680
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2025-11-16 18:40:57 +0100

    fix: source

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -1,8 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import {env} from '$env/dynamic/private'
+
 
 global.prisma;
 
-const prisma = global.prisma || new PrismaClient();
+
+const prisma = global.prisma || new PrismaClient({datasourceUrl:env.DATABASE_URL});
 if (process.env.NODE_ENV === 'development') {
 	global.prisma = prisma;
 }
